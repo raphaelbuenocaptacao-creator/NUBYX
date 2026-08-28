24c85624158bfcc120287fe2281dee4f0ea33abf
raphaelbuenocaptacao-creator <raphaelbueno.captacao@gmail.com>
2026-08-28 12:04:52 -0300

    Version PWA cache for secure NUBYX ID flow

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const CACHE='nubyx-v0.2.0-2050';
-const ASSETS=['./','./index.html','./styles.css','./future.css','./app.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='nubyx-v0.3.0-auth';
+const ASSETS=['./','./index.html','./styles.css','./future.css','./config.js','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET') return;
-  e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{const clone=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,clone)); return res;}).catch(()=>caches.match('./index.html'))));
+  const url=new URL(e.request.url);
+  if(url.origin!==self.location.origin) return;
+  e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{if(res&&res.ok){const clone=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,clone));} return res;}).catch(()=>caches.match('./index.html'))));
 });
