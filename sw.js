19dfce3b539fb3375173d1e648fbe75a29372abd
raphaelbuenocaptacao-creator <raphaelbueno.captacao@gmail.com>
2026-08-28 11:07:32 -0300

    Cache NUBYX 2050 visual assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='nubyx-v0.1.0';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='nubyx-v0.2.0-2050';
+const ASSETS=['./','./index.html','./styles.css','./future.css','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
